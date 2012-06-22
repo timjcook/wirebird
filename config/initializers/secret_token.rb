97f4630dd7c1cfebafec9278bd36f66c75856382
Tim Cook <tim@wakeless.net>
2012-06-22 17:22:48 +1000

    Added git repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wirebird::Application.config.secret_token = '5bd37b0855538187bcfc9f35e7da36f31da115b3a767ea164feeec2ecfc726c44b145fc17fdc8485aa236e2b4beeda35c83b49673fe007da2c95d87d4ba9e948'
