e674fc019ccb7e577dca0451db3ce0e7ef8dc1be
Tim Cook <tim@wakeless.net>
2012-10-20 00:57:00 +1100

    temp home screen

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ Wirebird::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'home#soon'
 
   # See how all your routes lay out with "rake routes"
 
