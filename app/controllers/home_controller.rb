d62ed68dec0980ff8cdedb4f19edfea5f972dd7c
Tim Cook <tim@wakeless.net>
2012-10-23 00:00:34 +1100

    fixed admin

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,10 @@
 class HomeController < ApplicationController
+
+  def index
+    @blog_posts = BlogPost.all
+    @admin = AdminUser.new(:email => 'test@wirebirdmelbourne.com')
+    @admin.save
+     
+  end
+
 end
---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,2 @@
+
+Time::DATE_FORMATS[:day_month_and_year] = "%d %B %Y"
